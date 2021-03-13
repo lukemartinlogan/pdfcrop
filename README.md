48ebda3d66e85c31fc9cab4f11a61fb2407a0660
lukemartinlogan <lukemartinlogan@gmail.com>
2021-03-13 17:22:03 -0600

    Add pdfcrop information

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # pdfcrop
+
+A simple wrapper around GhostWrapper for removing whitespace in pdfs.
+
+# Installation
+
+## For Regular Users
+```{bash}
+cd /path/to/pdfcrop  
+python3 setup.py sdist bdist_wheel  
+python3 -m pip install dist/*.whl --user
+rm -r dist build *.egg_info MANIFEST  
+```
+
+## For Developers
+
+```{bash}
+cd /path/to/pdfcrop  
+python3 setup.py develop --user
+```
+
+## Uninstallation
+
+```{bash}
+python3 -m pip uninstall pdfcrop
+```
---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="pdfcrop",
+    #packages=setuptools.find_packages(where='src'),
+    #package_dir = {'':'src'},
+    scripts=['bin/pdfcrop'],
+    version="0.0.1",
+    author="Luke Logan",
+    author_email="llogan@hawk.iit.edu",
+    description="A simple tool to crop whitespace in pdfs",
+    url="https://github.com/lukemartinlogan/pdfcrop",
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 0 - Pre-Alpha",
+        "Environment :: Other Environment",
+        "Intended Audience :: Users",
+        "License :: None",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Office",
+    ],
+    long_description=""
+)
